8fdb2b2fd616544662d00060b244316074312658
Stephen N <sndegwa.n@outlook.com>
2022-01-06 22:06:21 +0300

    chore: update application code
     * update applicatication code due to added features

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,20 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { HomePage, EditProfilePage } from "./pages";
+
 export default function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/edit-profile">
+          <EditProfilePage />
+        </Route>
+        <Route path="/view-profile">
+          <div>View profile</div>
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,14 +1,21 @@
 import { mount } from "enzyme";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from './App';
+import { HomePage } from "./pages";
 
 describe('renders learn react link', () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = mount(<App />);
+  beforeEach(() => wrapper = mount(<App />))
+
+  test("should have the Router and Route components", () => {
+    expect(wrapper.find(Router)).toHaveLength(1);
+
+    expect(wrapper.find(Route).prop("path")).toEqual("/");
   });
 
-  test("should render", () => {
-    expect(wrapper.find(App)).toHaveLength(1);
+  test("should show the HomePage component on render", () => {
+
+    expect(wrapper.find(HomePage)).toHaveLength(1);
   })
 });
